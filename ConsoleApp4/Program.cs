f18ab9fe0bac6d62560c630b345d0d9ee78480e9
soni-chanchal <chanchalsoni0722@gmail.com>
2023-05-14 13:03:14 +0530

    update

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Create1DArray();
+    }
+
+
+    public static void Create1DArray()
+    {
+        // 1-D Array
+        Console.WriteLine("Enter the size of array: ");
+        string len = Console.ReadLine();
+        int length = int.Parse(len);
+        int[] arr = new int[length];
+
+        // pushing element in array
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine("Enter the element: ");
+            string ele = Console.ReadLine();
+            arr[i] = int.Parse(ele);
+        }
+
+        // traverse and print element of array
+        Console.WriteLine();
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine($"element at {i} index = {arr[i]} ");
+        }
+
+    }
+}
